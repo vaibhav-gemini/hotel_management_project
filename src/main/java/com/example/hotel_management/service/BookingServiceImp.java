355a3597b5bed71d074bd98c78598f0a7dc4af4f
vaibhav-gemini <vaibhav.oberoi@geminisolutions.com>
2023-03-13 14:57:55 +0530

    Commiting BookingServiceImp

--- src/main/java/com/example/hotel_management/service/BookingServiceImp.java
+++ src/main/java/com/example/hotel_management/service/BookingServiceImp.java
@@ -24,7 +24,7 @@ public class BookingServiceImp implements BookingService{
     private RoomRepository roomRepository;
     @Autowired
     private RoomService roomService;
-    private int toGetRoomEntriesOnce = 0;
+    private int toGetSingleRoomEntries = 0;
 
     @Autowired
     private BookingRepository bookingRepository;
@@ -184,9 +184,9 @@ public class BookingServiceImp implements BookingService{
      */
     @Override
     public Booking methodToAllocateRoom(Booking booking, List<Room> roomList) throws DataNotFoundException {
-        if(toGetRoomEntriesOnce==0){
+        if(toGetSingleRoomEntries==0){
             roomService.addDefaultroom();
-            toGetRoomEntriesOnce++;
+            toGetSingleRoomEntries++;
         }
         String roomRequired = booking.getTypeOfRoomPrefered();
         List<Room> ansRooms = roomRepository.findRoomByTypeAndAvailability(roomRequired,true);
